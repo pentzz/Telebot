9659b2e75e23c3ae520230794dc7c104f8347803
pentzz <ofirbaranesad@gmail.com>
2023-10-06 19:02:23 +0300

    Update Telebot.py

--- Telebot.py
+++ Telebot.py
@@ -1,34 +1,42 @@
-import datetime
 import telebot
+from datetime import datetime
+import calendar
 
-TOKEN = "6659677963:AAHg4DNE6hqhVB6UidmoH81RGP1dVCEL4qQ"
+TOKEN = '6659677963:AAHg4DNE6hqhVB6UidmoH81RGP1dVCEL4qQ'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.send_message(message.chat.id, "שלום! ברוך הבא לבוט הכלכלה שלך. נתחיל: כמה אתה מצפה להרוויח בחודש?")
-    bot.register_next_step_handler(message, get_monthly_income)
+users_data = {}
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    msg = bot.reply_to(message, "שלום! אנא הכנס את ההכנסה הצפויה שלך בחודש.")
+    bot.register_next_step_handler(msg, get_monthly_income)
 
 def get_monthly_income(message):
-    monthly_income = float(message.text)
-    bot.send_message(message.chat.id, "איזה שכר שעתי יש לך?")
-    bot.register_next_step_handler(message, get_hourly_wage, monthly_income)
+    chat_id = message.chat.id
+    monthly_income = message.text
+    users_data[chat_id] = {'monthly_income': monthly_income}
+    msg = bot.reply_to(message, "כמה אתה רוצה שיהיה לך בשקלים בחודש?")
+    bot.register_next_step_handler(msg, get_desired_savings)
 
-def get_hourly_wage(message, monthly_income):
+def get_desired_savings(message):
+    chat_id = message.chat.id
+    desired_savings = message.text
+    users_data[chat_id]['desired_savings'] = desired_savings
+    msg = bot.reply_to(message, "אנא הכנס את השכר השעתי שלך.")
+    bot.register_next_step_handler(msg, get_hourly_wage)
+
+def get_hourly_wage(message):
+    chat_id = message.chat.id
     hourly_wage = float(message.text)
-    
-    # Calculate number of hours he needs to work in a month
-    monthly_hours_required = monthly_income / hourly_wage
+    users_data[chat_id]['hourly_wage'] = hourly_wage
 
-    # Get the current month to calculate number of working days
-    today = datetime.date.today()
-    working_days_in_month = len([1 for i in range(today.month) if datetime.date(today.year, today.month, i).weekday() < 5])
-    
-    hours_per_day = monthly_hours_required / working_days_in_month
-    shifts_per_week = hours_per_day / 8  # Assuming a shift is 8 hours
+    today = datetime.today()
+    last_day_of_month = calendar.monthrange(today.year, today.month)[1]
+    working_days_in_month = len([1 for i in range(1, last_day_of_month + 1) if datetime(today.year, today.month, i).weekday() < 5])
+    hours_needed = (float(users_data[chat_id]['monthly_income']) - float(users_data[chat_id]['desired_savings'])) / hourly_wage
+    hours_per_week = hours_needed / (working_days_in_month / 5)
 
-    bot.send_message(message.chat.id, f"תצטרך לעבוד {monthly_hours_required} שעות בחודש, כלומר {hours_per_day} שעות ביום ו-{shifts_per_week} משמרות בשבוע.")
-    
-    # Add code here to gather information on monthly expenses...
+    bot.reply_to(message, f"אתה צריך לעבוד כ-{hours_needed:.2f} שעות בחודש, כלומר כ-{hours_per_week:.2f} שעות בשבוע.")
 
 bot.polling()
